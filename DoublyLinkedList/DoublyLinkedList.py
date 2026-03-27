c806c8776de833cf9d6c3343ed505469db66f9db
Al Fattaul Islam <sahilabrar444@gmail.com>
2026-03-27 10:23:35 +0600

    insertAtEnd done

--- DoublyLinkedList/DoublyLinkedList.py
+++ DoublyLinkedList/DoublyLinkedList.py
@@ -3,3 +3,38 @@ class Node:
     self.info = value
     self.prev = None
     self.next = None
+    
+    
+class DoublyLinkedList:
+  def __init__(self):
+    self.head = None
+    
+  def insertAtEnd(self, value):
+    temp = Node(value)
+    if(self.head == None):  # noqa: E711
+      self.head = temp
+      return
+    
+    t = self.head
+    while(t.next != None):  # noqa: E711
+      t = t.next
+      
+    t.next = temp
+    temp.prev = t
+    
+    
+  def printDLL(self):
+    t = self.head
+    while(t.next != None):  # noqa: E711
+      print(t.info)
+      t = t.next
+    print(t.info)
+    
+    
+
+obj = DoublyLinkedList()
+obj.insertAtEnd(10)
+obj.insertAtEnd(20)
+obj.insertAtEnd(30)
+obj.insertAtEnd(40)
+obj.printDLL()
