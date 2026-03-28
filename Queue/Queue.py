3a3522de6c478361f8c1a35c66335053f8393ff2
Al Fattaul Islam <sahilabrar444@gmail.com>
2026-03-28 15:08:40 +0600

    queue done

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -10,6 +10,25 @@ class Queue:
     
   def delete(self):
     if (self.isEmpty()):
-      raise Exception("Queue is empty")
+      print("Queue is empty")
+      # raise Exception("Queue is empty") # For Python 3 ==> it is an another way to handle the error instead of print statement
     else:
       return self.items.pop(0)
+    
+
+q = Queue()
+
+q.insert(10)
+q.insert(20)
+q.insert(30)
+q.insert(40)
+q.insert(50)
+
+print(f"Queue before delete: {q.items}")
+print(q.delete())
+print(q.delete())
+print(q.delete())
+print(q.delete())
+print(q.delete())
+q.delete()
+print(f"Queue after delete: {q.items}")
